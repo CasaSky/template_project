e330dadbb54557b79feacd71fe438b679634dea9
Talal Tabia <talal.tabia.one@gmail.com>
2020-11-29 14:08:14 +0100

    add fg props

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,5 +1,7 @@
 create role template_user with login password 'G1E:bVwy|x12Z-=o';
 
+create user template_user with password 'G1E:bVwy|x12Z-=o' role postgres;
+
 create database template_db owner template_user;
 
 set role template_user;
